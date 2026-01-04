d325edd211455cfbc0621457996806bf7f93d7f8
MerubS <smerub@yahoo.com>
2026-01-03 20:41:41 -0600

    Avoid duplicate listing

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message)=>{
     if(message.type === "JOB_APPLIED") {
-        fetch("https://script.google.com/macros/s/AKfycbxXJX2PPDsuVRNpjauRCDRI1FyxoYKEOCfn7ngZNfqWsghuOp23OrKFfuFatj4UST1w/exec", {
+        fetch("https://script.google.com/macros/s/AKfycbzz43oeHsfh_P7XoLuCZZfikE8CPfuCwtAZoPyhLOnq5l7lo1GtJwQ--yry2v1ZUYm6/exec", {
             method:"POST",
             headers: {"CONTENT-TYPE": "application/json"},
             body: JSON.stringify(message.payload)
